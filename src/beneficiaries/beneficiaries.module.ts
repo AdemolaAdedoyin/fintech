450f861362bf85d715fe25467cdb47dabb6e9457
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-11 16:21:48 -0500

    feat: add beneficiaries module

---
+++ src/beneficiaries/beneficiaries.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { BeneficiariesController } from './beneficiaries.controller';
+import { BeneficiariesService } from './beneficiaries.service';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [BeneficiariesController],
+  providers: [BeneficiariesService],
+  exports: [BeneficiariesService],
+})
+export class BeneficiariesModule {}
