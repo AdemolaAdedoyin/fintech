91e0debd7e2b132fdacf6ec477701724a7d39cf1
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-11 16:32:57 -0500

    fix: provide users dependency to beneficiary auth guard

--- src/beneficiaries/beneficiaries.module.ts
+++ src/beneficiaries/beneficiaries.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 import { BeneficiariesController } from './beneficiaries.controller';
 import { BeneficiariesService } from './beneficiaries.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [BeneficiariesController],
   providers: [BeneficiariesService],
   exports: [BeneficiariesService],
