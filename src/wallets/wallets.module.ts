4c922bb80dc94c9e1178272fac04bfd09a15203c
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-11 12:25:18 -0500

    fix: provide guard user dependency in wallets module

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 import { WalletsController } from './wallets.controller';
 import { WalletsService } from './wallets.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [WalletsController],
   providers: [WalletsService],
   exports: [WalletsService],
