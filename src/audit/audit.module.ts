7420d3d9b4e647ffef891e76ee445a63f7479443
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-11 17:47:14 -0500

    fix: provide users dependency to audit auth guard

--- src/audit/audit.module.ts
+++ src/audit/audit.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 import { AuditController } from './audit.controller';
 import { AuditService } from './audit.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AuditController],
   providers: [AuditService],
   exports: [AuditService],
