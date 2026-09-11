3cee7ee487131b21d3cb5d489c52f207ce7b81eb
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-11 17:47:24 -0500

    fix: provide users dependency to reversal auth guard

--- src/reversals/reversals.module.ts
+++ src/reversals/reversals.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { LedgerModule } from '../ledger/ledger.module';
+import { UsersModule } from '../users/users.module';
 import { ReversalsController } from './reversals.controller';
 import { ReversalsService } from './reversals.service';
 
 @Module({
-  imports: [AuthModule, LedgerModule],
+  imports: [AuthModule, LedgerModule, UsersModule],
   controllers: [ReversalsController],
   providers: [ReversalsService],
   exports: [ReversalsService],
